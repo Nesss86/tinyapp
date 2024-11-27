88809566af682a7589d0ea525cb0a8b8371c29a6
Nesss86 <Nessymonster86@gmail.com>
2024-11-26 21:34:04 -0500

    Added redirect to express_server.js

--- express_server.js
+++ express_server.js
@@ -59,6 +59,8 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
+  const shortURL = generateRandomString(); 
+  const longURL = req.body.longURL; 
+  urlDatabase[shortURL] = longURL; 
+  res.redirect(`/urls/${shortURL}`);
+});  
